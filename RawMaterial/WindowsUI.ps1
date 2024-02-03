6fefe6951d9cc57845c3e47350462ba848e2371a
AndreusUltimus <1473025+andrewww@users.noreply.github.com>
2024-02-03 17:57:04 -0600

    System as well as app dark mode

--- RawMaterial/WindowsUI.ps1
+++ RawMaterial/WindowsUI.ps1
@@ -1,4 +1,5 @@
 
 # Dark mode
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
 
